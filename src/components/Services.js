526b5c65a9943f824f345a6d30b947ae7af8f243
Rushil <rushil13579@gmail.com>
2023-07-14 01:16:30 +0530

    Service Card

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import ServiceCard from './ServiceCard'
 
 function Services() {
   return (
     <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-10 z-10 relative">
-        <h1 className='font-bold text-3xl'>Services</h1>
+        <h1 className='font-bold text-3xl pb-8  '>Services</h1>
+        <ServiceCard name='Plumbing' 
+                     description=' Flow with Confidence, Your Trusted Plumbing Experts Plumbing Solutions that Keep Things Running Smoothly' 
+                     link='Link'/>
     </div>
   )
 }
